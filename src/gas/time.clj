6c99af5868dfdd63ec78e7aaa7628919037be87b
Andrés Gasson <gas@ateasystems.com>
2017-03-12 12:30:40 +1300

    added time fns

--- src/gas/time.clj
+++ src/gas/time.clj
@@ -6,3 +6,14 @@
 (def yesterday (-> (today) (minus (days 1)) to-sql-date))
 
 (def now (.getTime (java.util.Date.)))
+
+;;(def date-format (ctime-f/formatters :date))
+;;(def prettydate-format (ctime-f/formatter "EEE d, yyyy 'at' HH.mm. "))
+
+(def today (atom nil))
+
+;;(defn update-today []
+  (reset! today (ctime-f/unparse date-format (ctime-u/now))))
+
+;;(defn print-today []
+  (ctime-f/unparse (ctime-f/with-zone prettydate-format (ctime-u/default-time-zone)) (ctime-u/now)))
